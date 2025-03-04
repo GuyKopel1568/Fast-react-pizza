0bf73eb640dfc1abd6caf46a6488c446f1aa476b
GuyKopel1568 <guykopel1568@gmail.com>
2025-03-04 15:28:25 +0200

    Use loader from react-router-dom to get the data from the api

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,24 @@
+import { getMenu } from "../../services/apiRestaurant";
+import { useLoaderData } from "react-router-dom";
+import MenuItem from "./MenuItem";
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData();
+  console.log(menu);
+  return (
+    <ul>
+      {menu.map(pizza =>
+        <li key={pizza.id}>
+          <MenuItem pizza={pizza} />
+        </li>
+      )}
+    </ul>
+  );
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
