2215dd4c7f73a1d529def482443529c8b89664be
GuyKopel1568 <guykopel1568@gmail.com>
2025-03-08 11:57:35 +0200

    Add the global username to all the needed places

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
-import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
-
+import { useSelector } from "react-redux";
 // https://uibakery.io/regex-library/phone-number
 
 const isValidPhone = (str) =>
@@ -38,6 +37,7 @@ function CreateOrder() {
   const navigation = useNavigation();
   const formErrors = useActionData();
   const isSubmitting = navigation.state === "submitting";
+  const username = useSelector((state) => state.user.username);
 
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
@@ -52,7 +52,13 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center sm:justify-between">
           <label className="sm:basis-40">First Name</label>
-          <input type="text" name="customer" required className="input grow" />
+          <input
+            type="text"
+            name="customer"
+            defaultValue={username}
+            required
+            className="input grow"
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center sm:justify-between">
