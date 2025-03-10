17ec1b135c9b37626e7b56818f36c185e1597dc9
GuyKopel1568 <guykopel1568@gmail.com>
2025-03-10 12:18:11 +0200

    Integrate the Geolocation into the form

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,15 +1,16 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { getCart } from "../cart/cartSlice";
-import { createOrder } from "../../services/apiRestaurant";
+import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 
 import Button from "../../ui/Button";
 import Emptycart from "../cart/EmptyCart";
 import store from "../../store";
+import { createOrder } from "../../services/apiRestaurant";
+import { getCart } from "../cart/cartSlice";
 import { clearCart } from "../cart/cartSlice";
 import { getTotalPrice } from "../cart/cartSlice";
 import { formatCurrency } from "../../utils/helpers";
+import { fetchAddress } from "../user/userSlice";
 
 // https://uibakery.io/regex-library/phone-number
 
@@ -21,12 +22,24 @@ const isValidPhone = (str) =>
 function CreateOrder() {
   const navigation = useNavigation();
   const formErrors = useActionData();
-  const isSubmitting = navigation.state === "submitting";
-  const username = useSelector((state) => state.user.username);
-  const [withPriority, setWithPriority] = useState(false);
-
+  const dispatch = useDispatch();
   const cart = useSelector(getCart);
   const totalCartPrice = useSelector(getTotalPrice);
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+  } = useSelector((state) => state.user);
+
+  console.log(position);
+  console.log(addressStatus);
+
+  const isLoadingAddress = addressStatus === "loading";
+
+  const [withPriority, setWithPriority] = useState(false);
+
+  const isSubmitting = navigation.state === "submitting";
   const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
   const totalPrice = totalCartPrice + priorityPrice;
 
@@ -63,16 +76,33 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center sm:justify-between">
+        <div className="relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center sm:justify-between">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input
               type="text"
               name="address"
+              disabled={isLoadingAddress}
+              defaultValue={address}
               required
               className="input w-full"
             />
           </div>
+
+          {!position.latitude && !position.longitude && (
+            <span className="absolute right-[3px] z-50">
+              <Button
+                disabled={isLoadingAddress}
+                type={"small"}
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(fetchAddress());
+                }}
+              >
+                Get Position
+              </Button>
+            </span>
+          )}
         </div>
 
         <div className="mb-12 flex items-center gap-5">
